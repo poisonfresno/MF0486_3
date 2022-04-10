259554ac420be87ec43da6d1f1133847d70182fc
prunell <54021880+poisonfresno@users.noreply.github.com>
2022-04-10 15:30:38 +0100

    Add files via upload

--- masterScript.bat
+++ masterScript.bat
@@ -12,6 +12,10 @@ clear
 	echo "2. Upgrade"
 	echo "3. Puertos abiertos"
 	echo "4. Autoremover paquetes (purgar)"
+	echo "5. Auditar sistema con Lynis"
+	echo "6. Eliminar paquetes obsoletos"
+	echo "7. Información del sistema"
+	echo ""
 	echo "99. Salir"
 	echo ""
 	
@@ -23,12 +27,10 @@ case $opcion in
 
 	1) echo "Actualizando"
 	apt-get update
-	read foo
 	;;
 
 	2) echo "Upgrading"
 	apt-get upgrade -y
-	read foo
 	;;
 
 	3) echo "Puertos abiertos"
@@ -38,6 +40,19 @@ case $opcion in
 	
 	4) echo "Limpiando paquetes"
 	apt autoremove
+	;;
+	
+	5) echo "Auditando sistema..."
+	lynis audit system > lynisReport.txt
+	;;
+	
+	6) echo "Limpiando el sistema..."
+	apt-get clean
+	apt-get autoremove
+	;;
+	
+	7) echo "Información de red:"
+	ifconfig -a
 	read foo
 	;;
 
